04587eb7e04c24281b9c6293c152094f3cea5870
DESKTOP-Q9KG21M\user <basenji94@gmail.com>
2022-06-21 17:42:50 +0900

    초기환경 세팅

---
+++ src/main/java/com/example/lesson01/Data.java
@@ -0,0 +1,23 @@
+package com.example.lesson01;
+
+// 일반 자바 bean
+public class Data {
+	private int id;
+	private String name;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
